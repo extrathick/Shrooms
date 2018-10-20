9dfb18f70d27913e95f046d96614e5871981aa1b
Justin Pham <jpham79@hawaii.edu>
2018-10-19 23:09:25 -1000

    React App Setup

---
+++ src/App.js
@@ -0,0 +1,30 @@
+/* THIS IS THE ROOT OF THE APP */
+
+import React, { Component } from 'react';
+import logo from './pics/logo.svg'; //Import components/Images from folders like this
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App; //For each component you make, you'll need to export it.
