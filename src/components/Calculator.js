9f3e5f418a2ffa9e454de5964d0491bf81720d7a
Tyler Dorn <tylergdorn@gmail.com>
2018-11-27 18:39:48 -1000

    calculator nearly working

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,20 +7,40 @@ export default class Calculator extends Component {
         super(props);
         this.look = new lookup();
         this.list = this.look.getCategories();
+        // this gets rid of "class". the order of getCategories should not change for any reason, so this is an okay hack. 
+        // the calculator tells you if it's edible so this is okay for the most part. 
+        this.list.splice(0, 1);
+        // calcval keeps track of all the values selected in the calculator
+        // this should potentially be state, but this keeps redraws down.
         this.calcVal = [];
+        this.state = {
+            edibility: .5
+        }
     }
 
-    returnMethod = (keyVal) => {
-        this.calcVal.push(keyVal);
-        console.log(this.calcVal);
+    // remove here is a bit of a hack. if it's true i remove the item, if it's false i add it. 
+    returnMethod = (keyVal, remove) => {
+        if(remove){
+            for(let i in this.calcVal){
+                // it turns out there's no good way to do this in javascript. options are to either look at a 3rd party library, do this, or do JSON.stringify on both. 
+                if(this.calcVal[i].value === keyVal.value && this.calcVal[i].key === keyVal.key){
+                    this.calcVal.splice(i, 1);
+                }
+            }
+        }
+        else {
+            this.calcVal.push(keyVal);
+        }
     }
 
+    // this renders out the dom, mostly just calcsubclasses
     render() {
         let listItems = this.list.map((listItem) =>
             <CalcSubclass key={listItem} category={listItem} ret={this.returnMethod} />
         );
         return (
             <div>
+                <h3>Chance of Edibility: {this.state.edibility * 100}%</h3>
                 {listItems}
             </div>
         )
--- src/components/calculator/CalcSubclass.js
+++ src/components/calculator/CalcSubclass.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 import lookup from '../../lib/csvValueLookup';
 import styled from 'styled-components';
 
+// The title for each section of the calculator
 const Title = styled.p `
     color: white;
 `;
 
+// The list of all the buttons
 const List = styled.div `
     display: flex;
     justify-content: space-evenly;
@@ -16,6 +18,7 @@ export default class CalcSubclass extends Component {
         super(props);
         this.look = new lookup();
         let list = this.look.getAllPossible(props.category);
+        // just get the 'long' versions from the list of possible values
         let shortList = list.map(item => {
             return item.value;
         });
@@ -25,6 +28,7 @@ export default class CalcSubclass extends Component {
             chosen: false
         }
     }
+    // this is what happens when you click on an item. We set the value in calculator and also change the style. This also helps disable the buttons
     call = (arg, clicked) => {
         if(!clicked){
             this.setState({
@@ -33,10 +37,14 @@ export default class CalcSubclass extends Component {
             this.props.ret({
                 key: this.state.category,
                 value: arg.target.value
-            });
+            }, clicked);
             arg.target.style='color: red';
         }
         else {
+            this.props.ret({
+                key: this.state.category,
+                value: arg.target.value
+            }, clicked);
             this.setState({
                 chosen: false
             });
@@ -44,11 +52,12 @@ export default class CalcSubclass extends Component {
         }
     }
     render() {
-        // i'll probably switch the p and the ul to be diffo components but that is for later. 
         return (
             <div>
+            {/* This renders out the title, which is defined above */}
                 <Title>{this.state.category}</Title>
                 <List>
+                    {/* This is the bulk of our work, in rendering the ItemButton, which handles a lot of the log. Disabled is to make it disabled later. click is our return method */}
                     {this.state.items.map((item) => {
                         return <ItemButton key={item} value={item} click={this.call} disabled={this.state.chosen}>{item}</ItemButton>
                     })}
@@ -58,7 +67,7 @@ export default class CalcSubclass extends Component {
     }
 }
 
-
+// our buttons for the calculator
 const Button = styled.button `
     display: inline-block;
     padding-left: 1vw;
@@ -80,12 +89,15 @@ class ItemButton extends Component {
       };
     }
     
+    // this is how we track if the button was previously clicked, so that we can go ahead and keep it enabled while disabling other buttons
     clicc = (event) =>{
         this.props.click(event, this.state.clicked);
         this.setState({clicked: !this.state.clicked});
     }
 
   render() {
+      // disabled here is either false or the passed value of disabled. This allows us to selectively disable all but one element.
+      // this.props.children is the elements inside the button. Since we're using a styled component, we need to do this. 
     return (
         <Button onClick={this.clicc} value={this.props.value} disabled={this.state.clicked ? false : this.props.disabled}>{this.props.children}</Button>
     )
